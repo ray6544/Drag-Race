bb0d92914493c5d9fed434049ad902b5879175fd
ray6544 <u.shahid6544@gmail.com>
2022-03-12 12:39:49 +0500

    env01p3

---
+++ Drag Race Sim/Assets/Anime Particles/Demo/Scripts/CreateFireballs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFireballs : MonoBehaviour {
+
+    public Rigidbody Fireball;
+
+    private bool CreateInstances = true;
+    private Rigidbody Instance;
+
+	void Start () {
+        InvokeRepeating("Create", 1, 1);
+	}
+    void Update()
+    {
+        if(Instance == null)
+        {
+            CreateInstances = true;
+        }
+    }
+
+    void Create()
+    {
+        if (CreateInstances)
+        {
+            Instance = Instantiate(Fireball, transform.position, transform.rotation); 
+            CreateInstances = false;
+        }
+    }
+}
---
+++ Drag Race Sim/Assets/Anime Particles/Demo/Scripts/Fireballs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireballs : MonoBehaviour
+{
+    public GameObject ExplosionPrefab;
+    public float DestroyExplosion = 3.0f;
+
+    void OnCollisionEnter(Collision col)
+    {
+        var exp = Instantiate(ExplosionPrefab, transform.position, transform.rotation);
+        Destroy(exp, DestroyExplosion);
+        Destroy(gameObject);
+    }
+}
